5d5cdd23b4f40efdff83168142d5dd59df76c90e
CrashBandiCoin <88516022+adamChafqani@users.noreply.github.com>
2021-09-27 03:49:43 +0200

    initial commit

--- src/style/Global.tsx
+++ src/style/Global.tsx
@@ -11,14 +11,19 @@ const GlobalStyle = createGlobalStyle`
   * {
     font-family: 'Kanit', sans-serif;
   }
-  body {
-    background-color: ${({ theme }) => theme.colors.background};
+  body {  
+    background-color: #f39f86;
+    background-image: linear-gradient(315deg, #f39f86 0%, #f9d976 74%);
+  ;
 
     img {
       height: auto;
       max-width: 100%;
     }
+    
   }
+  
+  
 `
 
 export default GlobalStyle
